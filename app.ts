d600747f243886efa1c0fe7f70da1d6f8b6d1f87
Denny Hong <hong961127@gmail.com>
2021-08-25 14:25:27 -0400

    Configure auto reload

---
+++ app.ts
@@ -0,0 +1,12 @@
+function add(n1: number, n2: number) {
+  console.log(typeof n1);
+  console.log(typeof n2);
+  return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+
+const result = add(num1, num2);
+
+console.log(result);
